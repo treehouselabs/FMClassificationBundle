28904288853585a2e8562e6668cdaa6a7cc99d86
Cas Leentfaar <git@casleentfaar.com>
2014-02-27 10:27:49 +0100

    adjusted BooleanNormalizer to be more strict

--- src/FM/ClassificationBundle/Normalizer/BooleanNormalizer.php
+++ src/FM/ClassificationBundle/Normalizer/BooleanNormalizer.php
@@ -4,6 +4,13 @@ namespace FM\ClassificationBundle\Normalizer;
 
 class BooleanNormalizer implements NormalizerInterface
 {
+    protected $trueValues;
+
+    public function __construct(array $trueValues = ['y', 'yes'])
+    {
+        $this->trueValues = $trueValues;
+    }
+
     /**
      * @param $value
      * @return bool|null
@@ -14,8 +21,20 @@ class BooleanNormalizer implements NormalizerInterface
             return null;
         }
 
-        if (is_scalar($value)) {
-            return (bool) $value;
+        if (is_bool($value)) {
+            return $value;
+        }
+
+        if (is_numeric($value) && intval($value) === 1) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            foreach ($this->trueValues as $trueValue) {
+                if (strcasecmp($value, $trueValue) == 0) {
+                    return true;
+                }
+            }
         }
 
         return false;
