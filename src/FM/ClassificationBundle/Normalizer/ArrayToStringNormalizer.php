e7322497985aebd13355f83f3bfc434e8f6a8f1e
Cas Leentfaar <git@casleentfaar.com>
2014-02-27 17:00:43 +0100

    fixed glue issues with ArrayToStringNormalizer; updated tests to reflect the changes

--- src/FM/ClassificationBundle/Normalizer/ArrayToStringNormalizer.php
+++ src/FM/ClassificationBundle/Normalizer/ArrayToStringNormalizer.php
@@ -4,6 +4,11 @@ namespace FM\ClassificationBundle\Normalizer;
 
 class ArrayToStringNormalizer implements NormalizerInterface
 {
+    public function __construct($glue = '')
+    {
+        $this->glue = $glue;
+    }
+
     public function normalize($value)
     {
         if (is_null($value)) {
@@ -21,18 +26,26 @@ class ArrayToStringNormalizer implements NormalizerInterface
         return null;
     }
 
-    protected function implodeRecursively(array $array, $glue = '')
+    protected function implodeRecursively(array $array)
     {
         $imploded = '';
 
         foreach ($array as $item) {
             if (is_array($item)) {
-                $imploded .= $this->implodeRecursively($item, $glue) . $glue;
+                $imploded .= $this->implodeRecursively($item) . $this->glue;
             } elseif (is_scalar($item)) {
-                $imploded .= $item . $glue;
+                if ($imploded == '') {
+                    $imploded .= $item . $this->glue;
+                } else {
+                    $imploded .= $item . $this->glue;
+                }
             } else {
                 throw new \OutOfBoundsException(sprintf("Can't implode a non-array: %s", var_export($item, true)));
             }
+        }
+
+        if ($this->glue != '') {
+            $imploded = substr($imploded, 0, -(strlen($this->glue)));
         }
 
         return $imploded;
--- src/FM/ClassificationBundle/Tests/Normalizer/ArrayToStringNormalizerTest.php
+++ src/FM/ClassificationBundle/Tests/Normalizer/ArrayToStringNormalizerTest.php
@@ -18,12 +18,33 @@ class ArrayToStringNormalizerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $result);
     }
 
+    /**
+     * @dataProvider normalizeDataProviderWithGlue
+     */
+    public function testNormalizeWithGlue($glue, $value, $expected)
+    {
+        $normalizer = new ArrayToStringNormalizer($glue);
+
+        $result = $normalizer->normalize($value);
+
+        $this->assertEquals($expected, $result);
+    }
+
     public function normalizeDataProvider()
     {
         return [
             [[1, 2, 3], '123'], // array normalizing
             [[[1], [2], 3 => 3], '123'], // normalizing combination of arrays and scalars
             [new \stdClass(), null], // not an array or string
+        ];
+    }
+
+    public function normalizeDataProviderWithGlue()
+    {
+        return [
+            ['*', [1, 2, 3], '1*2*3'], // array normalizing
+            ['*', [[1], [2], 3 => 3], '1*2*3'], // normalizing combination of arrays and scalars
+            ['*', new \stdClass(), null], // not an array or string
         ];
     }
 }
